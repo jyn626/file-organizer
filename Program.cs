ba0a5b2337632bf10a26329930921b9043b2fa42
jyn626 <tuyor.jynn@gmail.com>
2026-08-26 16:50:47 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace fileproj
+{
+  class Program
+  {
+    static void Main(String[] args)
+    {
+      Console.WriteLine("Enter the folder path to organize.");
+      string folderPath = Console.ReadLine();
+
+      // TODO: handle this in a better way
+      if (String.IsNullOrEmpty(folderPath))
+      {
+        Console.WriteLine("Please enter the folder path.");
+        return;
+      }
+
+      folderPath = folderPath.Trim();
+
+      Console.WriteLine($"Folder path to organize: {folderPath}");
+
+      // TODO: maybe start the program again instead of returning and ending the program?
+      if (!Directory.Exists(folderPath))
+      {
+        Console.WriteLine("Path does not exists.");
+        return;
+      }
+
+      Console.WriteLine("-  Folder found. Ready to scan.");
+
+    }
+  }
+}
