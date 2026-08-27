e74baec70b254aa018237b19361c0bb5038e8aa1
jyn626 <tuyor.jynn@gmail.com>
2026-08-27 18:09:12 +0800

    get files extention

--- Program.cs
+++ Program.cs
@@ -14,6 +14,26 @@
       return Files.Count;
     }
 
+    // <summary>
+    // Gets every extentions for each file in the `Files` list.
+    // </summary>
+    // <returns>List of extensions.</returns>
+    public static List<string> GetExtensions()
+    {
+      // ! TODO: Handle files with no extension (example: README)
+      List<string> exts = new();
+      foreach (string file in Files)
+      {
+        string ext = Path.GetExtension(file);
+
+        // Convert extensions to a consistent format
+        ext = ext.ToLower();
+        exts.Add(ext);
+      }
+
+      return exts;
+    }
+
   }
 
   class Program
@@ -50,7 +70,12 @@
       // store every file name in the list
       foreach (string file in files)
       {
-        FileManager.StoreFile(Path.GetFileName(file));
+        string fileName = Path.GetFileName(file);
+        string ext = Path.GetExtension(file);
+        FileManager.StoreFile(fileName);
+
+        // Print: file name → extension 
+        Console.WriteLine($"{fileName} -> {ext}");
       }
 
       Console.WriteLine($"Total files found: {FileManager.TotalFilesFound()}");
